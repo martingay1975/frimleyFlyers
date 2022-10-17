5ce33219399d0d4f00464faefad95eedac5da0fc
Martin Gay <martingay1975@gmail.com>
2022-10-17 18:47:46 +0100

    update

--- parkrunScraper/typings/index.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="globals/cheerio/index.d.ts" />
-/// <reference path="globals/express/index.d.ts" />
-/// <reference path="globals/moment/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="globals/request/index.d.ts" />
